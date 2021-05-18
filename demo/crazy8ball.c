11524005d6c455a1865ba7bcd6252e8a01ec4b0d
brownsnig <63370236+brownsnig@users.noreply.github.com>
2021-05-18 01:52:56 -0700

    iIiIi will always love u poolsetup

--- demo/crazy8ball.c
+++ demo/crazy8ball.c
@@ -17,6 +17,10 @@
 
 const vector_t LOW_LEFT_CORNER = {0, 0};
 const vector_t HIGH_RIGHT_CORNER = {1000, 600};
+const double CIRCLE_POINTS = 30;
+const double BALL_RADIUS = 15;
+const double TABLE_MASS = INFINITY;
+const double BALL_MASS = 10;
 
 
 // body_t *init_stick(int pts, double radius, double mass, rgb_color_t rgb_color) {
@@ -56,27 +60,48 @@ list_t *rect_init(double width, double height) {
     return rect;
 }
 
+list_t *circle_init(double radius){
+    list_t *circle_list = list_init(CIRCLE_POINTS, (free_func_t) free);
+    for (size_t i = 0; i < CIRCLE_POINTS - 1; i++) {
+        double angle = 2 * M_PI * i / CIRCLE_POINTS;
+        vector_t *v = malloc(sizeof(vector_t));
+        *v = (vector_t) {radius * cos(angle), radius * sin(angle)};
+        list_add(circle_list, (void *) v);
+    }
+
+    return circle_list;
+}
+
+void game_setup(scene_t *scene){
+    list_t *rect = rect_init(1000, 600);
+    SDL_Surface *table_image = IMG_Load("images/pool_table.png");
+    body_t *pool_table = body_init_with_info(rect, TABLE_MASS, (rgb_color_t) {0,0,0}, table_image, 800, 480, "POOL_TABLE", (free_func_t) free);
+    body_set_centroid(pool_table, (vector_t) {500, 300});
+    scene_add_body(scene, pool_table);
+
+    for(int i = 1; i <= 16; i++){
+        char name[20];
+        snprintf(name, 20, "images/ball_%d.png", i);
+        SDL_Surface *ball_image = IMG_Load(name);
+        body_t *pool_ball = body_init_with_info(circle_init(BALL_RADIUS), BALL_MASS, (rgb_color_t) {0,0,0}, ball_image, 
+                                        2*BALL_RADIUS, 2*BALL_RADIUS, "POOL_BALL", (free_func_t) free);
+        body_set_centroid(pool_ball, (vector_t) {i*10, i*15});
+        scene_add_body(scene, pool_ball);
+    }
+
+}
+
+
+
 int main(){
     scene_t *scene = scene_init();
+    game_setup(scene);
     SDL_Renderer *renderer = sdl_init(LOW_LEFT_CORNER, HIGH_RIGHT_CORNER);
-    list_t *rect = rect_init(1000, 600);
     SDL_SetRenderDrawColor(renderer, 0, 255, 255, 0);
-    SDL_Surface *image = IMG_Load("demo/small.png");
-    SDL_Surface *image2 = IMG_Load("demo/pool_table.png");
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
-    SDL_Texture *texture2 = SDL_CreateTextureFromSurface(renderer, image2);
-    
-    body_t *ditto_bod = body_init_with_info(rect, 10, (rgb_color_t) {0,0,0}, image2, 800, 480, NULL, free);
-    body_set_centroid(ditto_bod, (vector_t) {500, 300});
-    scene_add_body(scene, ditto_bod);
     
     while (!sdl_is_done()){
         sdl_render_scene(scene);
-        scene_tick(scene, time_since_last_tick());
-        //SDL_RenderCopy(renderer, texture2, NULL, NULL);  
-        //SDL_RenderCopy(renderer, texture, NULL, NULL);
-        //SDL_RenderPresent(renderer);
-        // free(boundary);   
+        scene_tick(scene, time_since_last_tick());  
     }
     scene_free(scene);
 }
