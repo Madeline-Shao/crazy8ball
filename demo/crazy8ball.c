6928684e301e8fc5aeb33b5c51578b217e774098
Patrick Rim <62685408+patrickqrim@users.noreply.github.com>
2021-05-26 22:37:54 -0700

    more fix NEVER GONNA SAY GOODBYE

--- demo/crazy8ball.c
+++ demo/crazy8ball.c
@@ -56,6 +56,7 @@ const double CUE_STICK_DEFAULT_Y = 177;
 const vector_t VELOCITY_THRESHOLD = {0.5, 0.5};
 const double TINY_CONSTANT = 0.8;
 const double PULL_FACTOR_ADJUSTMENT_CONSTANT = 47;
+const double SIZE_POWERDOWN_ADJUSTMENT_SCALE_FACTOR = 1.2;
 
 body_t *get_object(scene_t *scene, char *name){
     for (int i = 0; i < scene_bodies(scene); i++) {
@@ -487,9 +488,9 @@ void gameplay_handler(scene_t *scene, TTF_Font *font) {
                 game_state_set_ghost_powerup(game_state, true);
                 applied_power = true;
             }
-            else if (power_rand > 0){
+            else if (power_rand > 0.9 && power_rand < 0.95){
                 // powerdown 1
-                add_size_powerdown(scene, 2 * HOLE_RADIUS);
+                add_size_powerdown(scene, SIZE_POWERDOWN_ADJUSTMENT_SCALE_FACTOR * BALL_RADIUS);
                 game_state_set_size_powerdown(game_state, true);
                 applied_power = true;
             }
@@ -498,7 +499,6 @@ void gameplay_handler(scene_t *scene, TTF_Font *font) {
                 applied_power = true;
             }
         }
-
 
         body_t *ball = list_get(balls_sunk, i);
         // cue ball is sunk
